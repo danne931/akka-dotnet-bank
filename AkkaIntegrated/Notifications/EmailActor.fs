9fe2403052d37fa32604afdfafd1c0a10855b6fb
Daniel Eisenbarger <danne9.31@gmail.com>
2023-08-28 21:42:31 -0700

    use PipeTo in email actor for higher throughput

--- AkkaIntegrated/Notifications/EmailActor.fs
+++ AkkaIntegrated/Notifications/EmailActor.fs
@@ -13,7 +13,6 @@ open Lib.ActivePatterns
 open Lib.Types
 open ActorUtil
 open Bank.Transfer.Domain
-open BillingStatement
 
 type EmailMessage =
    | AccountOpen of AccountState
@@ -55,7 +54,7 @@ let private createClient (bearerToken: string) =
 
    client
 
-let start (system: ActorSystem) : IActorRef<EmailMessage> =
+let start (system: ActorSystem) =
    let emailBearerToken = Environment.GetEnvironmentVariable("EmailBearerToken")
 
    let client =
@@ -63,75 +62,80 @@ let start (system: ActorSystem) : IActorRef<EmailMessage> =
       | true -> None
       | false -> Some(createClient emailBearerToken)
 
-   let handler (ctx: Actor<_>) (msg: EmailMessage) = actor {
-      let emailData =
+   let handler (ctx: Actor<_>) (msg: obj) =
+      match msg with
+      | :? Result<HttpResponseMessage, string> as msg ->
          match msg with
-         | AccountOpen account -> {
-            event = "account-opened"
-            email = account.Email
-            data = {| firstName = account.FirstName |}
-           }
-         | AccountClose account -> {
-            event = "account-closed"
-            email = account.Email
-            data = {| firstName = account.FirstName |}
-           }
-         // TODO: Include link to view statement
-         | BillingStatement account -> {
-            event = "billing-statement"
-            email = account.Email
-            data = {| |}
-           }
-         | DebitDeclined(reason, account) ->
-            let o = {
-               event = "debit-declined"
+         | Error errMsg ->
+            logError ctx errMsg
+            unhandled ()
+         | Ok _ -> ignored ()
+      | :? EmailMessage as msg ->
+         let emailData =
+            match msg with
+            | AccountOpen account -> {
+               event = "account-opened"
                email = account.Email
-               data = {| reason = reason |}
-            }
-
-            match reason with
-            | Contains "InsufficientBalance" -> {
-               o with
-                  data = {|
-                     reason =
-                        $"Your account has insufficient funds. 
-                          Your balance is ${account.Balance}"
-                  |}
+               data = {| firstName = account.FirstName |}
               }
-            | Contains "ExceededDailyDebit" -> {
-               o with
-                  data = {|
-                     reason =
-                        $"You have spent ${account.DailyDebitAccrued} today. 
-                          Your daily debit limit is set to ${account.DailyDebitLimit}."
-                  |}
+            | AccountClose account -> {
+               event = "account-closed"
+               email = account.Email
+               data = {| firstName = account.FirstName |}
               }
-            | _ -> o
-         | TransferDeposited(evt, account) -> {
-            event = "transfer-deposited"
-            email = account.Email
-            data = {|
-               firstName = account.FirstName
-               amount = $"${evt.Data.DepositedAmount}"
-               origin = evt.Data.Origin
-            |}
-           }
+            // TODO: Include link to view statement
+            | BillingStatement account -> {
+               event = "billing-statement"
+               email = account.Email
+               data = {| |}
+              }
+            | DebitDeclined(reason, account) ->
+               let o = {
+                  event = "debit-declined"
+                  email = account.Email
+                  data = {| reason = reason |}
+               }
 
-      if client.IsNone then
-         logWarning ctx "EmailBearerToken not set.  Will not send email."
-         return ignored ()
-      else
-         let! res = sendEmail client.Value emailData |> Async.AwaitTask
+               match reason with
+               | Contains "InsufficientBalance" -> {
+                  o with
+                     data = {|
+                        reason =
+                           $"Your account has insufficient funds. 
+                             Your balance is ${account.Balance}"
+                     |}
+                 }
+               | Contains "ExceededDailyDebit" -> {
+                  o with
+                     data = {|
+                        reason =
+                           $"You have spent ${account.DailyDebitAccrued} today. 
+                             Your daily debit limit is set to ${account.DailyDebitLimit}."
+                     |}
+                 }
+               | _ -> o
+            | TransferDeposited(evt, account) -> {
+               event = "transfer-deposited"
+               email = account.Email
+               data = {|
+                  firstName = account.FirstName
+                  amount = $"${evt.Data.DepositedAmount}"
+                  origin = evt.Data.Origin
+               |}
+              }
 
-         return
-            match res with
-            | Error errMsg ->
-               logError ctx errMsg
-               unhandled ()
-            | Ok _ -> ignored ()
-   }
+         if client.IsNone then
+            logWarning ctx "EmailBearerToken not set.  Will not send email."
+         else
+            sendEmail client.Value emailData |> Async.AwaitTask
+            |!> retype ctx.Self
 
-   spawn system ActorMetadata.email.Name (props (actorOf2 handler))
+         ignored ()
+      | msg ->
+         logError ctx $"Unknown msg {msg}"
+         unhandled ()
+
+   spawn system ActorMetadata.email.Name (props <| actorOf2 handler)
 
 let get (system: ActorSystem) : IActorRef<EmailMessage> =
    typed <| ActorRegistry.For(system).Get<ActorMetadata.EmailMarker>()
