e0164cca3b1ab356ced32bb0bbad85e6a3686f3d
Daniel Eisenbarger <danne9.31@gmail.com>
2025-01-23 21:20:12 -0800

    fix AmountPerCommand rule with a lower or upper bound higher than an existing AmountDailyLimit rule not being detected as a conflict when attempting to create the AmountPerCommand rule after the AmountDailyLimit rule

--- Test.SharedClientServer/tests/CommandApproval.fs
+++ Test.SharedClientServer/tests/CommandApproval.fs
@@ -355,4 +355,209 @@ let tests =
 
          Expect.isFalse overlap "(6, 10) should not overlap with (11, 20)"
       }
+
+      test
+         "Daily Limit expected to be greater than amount based rules configured for the same command type" {
+         let existingRule = {
+            RuleId = Guid.NewGuid() |> CommandApprovalRuleId
+            OrgId = Stub.orgId
+            CommandType = ApprovableCommandType.FulfillPlatformPayment
+            Criteria =
+               Criteria.AmountPerCommand {
+                  LowerBound = None
+                  UpperBound = Some 50m
+               }
+            Approvers = [ Approver.AnyAdmin ]
+         }
+
+         let newRule = {
+            RuleId = Guid.NewGuid() |> CommandApprovalRuleId
+            OrgId = Stub.orgId
+            CommandType = ApprovableCommandType.FulfillPlatformPayment
+            Criteria = Criteria.AmountDailyLimit 60m
+            Approvers = [ Approver.AnyAdmin ]
+         }
+
+         let conflicts =
+            CommandApprovalRule.newRuleCriteriaConflictsWithExistingRule
+               [ existingRule ]
+               newRule
+
+         Expect.isFalse
+            conflicts
+            "Should be no conflict when daily limit (60) > upper bound (50) of existing rule"
+
+         let newRule = {
+            newRule with
+               Criteria = Criteria.AmountDailyLimit 40m
+         }
+
+         let conflicts =
+            CommandApprovalRule.newRuleCriteriaConflictsWithExistingRule
+               [ existingRule ]
+               newRule
+
+         Expect.isTrue
+            conflicts
+            "Should conflict when daily limit (40) < upper bound (50) of existing rule"
+
+         let existingRule = {
+            existingRule with
+               Criteria =
+                  Criteria.AmountPerCommand {
+                     LowerBound = Some 10m
+                     UpperBound = None
+                  }
+         }
+
+         let conflicts =
+            CommandApprovalRule.newRuleCriteriaConflictsWithExistingRule
+               [ existingRule ]
+               newRule
+
+         Expect.isFalse
+            conflicts
+            "Should be no conflict when daily limit (40) > lower bound (10) of existing rule"
+
+         let existingRule = {
+            existingRule with
+               Criteria =
+                  Criteria.AmountPerCommand {
+                     LowerBound = Some 50m
+                     UpperBound = None
+                  }
+         }
+
+         let conflicts =
+            CommandApprovalRule.newRuleCriteriaConflictsWithExistingRule
+               [ existingRule ]
+               newRule
+
+         Expect.isTrue
+            conflicts
+            "Should conflict when daily limit (40) < lower bound (50) of existing rule"
+
+         let existingRule = {
+            existingRule with
+               Criteria =
+                  Criteria.AmountPerCommand {
+                     LowerBound = Some 10m
+                     UpperBound = Some 30m
+                  }
+         }
+
+         let newRule = {
+            newRule with
+               Criteria = Criteria.AmountDailyLimit 20m
+         }
+
+         let conflicts =
+            CommandApprovalRule.newRuleCriteriaConflictsWithExistingRule
+               [ existingRule ]
+               newRule
+
+         Expect.isTrue
+            conflicts
+            "Should conflict when daily limit (20) between lower & upper bound of existing rule"
+
+         let newRule = {
+            newRule with
+               Criteria = Criteria.AmountDailyLimit 40m
+         }
+
+         let conflicts =
+            CommandApprovalRule.newRuleCriteriaConflictsWithExistingRule
+               [ existingRule ]
+               newRule
+
+         Expect.isFalse
+            conflicts
+            "Should not conflict when daily limit (40) above upper bound of existing rule"
+      }
+
+      test
+         "AmountPerCommand based rules expected to have amounts lower than Daily Limit rules configured for the same command type" {
+         let existingRule = {
+            RuleId = Guid.NewGuid() |> CommandApprovalRuleId
+            OrgId = Stub.orgId
+            CommandType = ApprovableCommandType.FulfillPlatformPayment
+            Criteria = Criteria.AmountDailyLimit 60m
+            Approvers = [ Approver.AnyAdmin ]
+         }
+
+         let newRule = {
+            RuleId = Guid.NewGuid() |> CommandApprovalRuleId
+            OrgId = Stub.orgId
+            CommandType = ApprovableCommandType.FulfillPlatformPayment
+            Criteria =
+               Criteria.AmountPerCommand {
+                  LowerBound = None
+                  UpperBound = Some 50m
+               }
+            Approvers = [ Approver.AnyAdmin ]
+         }
+
+         let conflicts =
+            CommandApprovalRule.newRuleCriteriaConflictsWithExistingRule
+               [ existingRule ]
+               newRule
+
+         Expect.isFalse
+            conflicts
+            "Should be no conflict when upper bound (50) < daily limit (60) of existing rule"
+
+         let newRule = {
+            newRule with
+               Criteria =
+                  Criteria.AmountPerCommand {
+                     LowerBound = None
+                     UpperBound = Some 70m
+                  }
+         }
+
+         let conflicts =
+            CommandApprovalRule.newRuleCriteriaConflictsWithExistingRule
+               [ existingRule ]
+               newRule
+
+         Expect.isTrue
+            conflicts
+            "Should conflict when upper bound (70) > daily limit (60) of existing rule"
+
+         let newRule = {
+            newRule with
+               Criteria =
+                  Criteria.AmountPerCommand {
+                     LowerBound = Some 50m
+                     UpperBound = Some 70m
+                  }
+         }
+
+         let conflicts =
+            CommandApprovalRule.newRuleCriteriaConflictsWithExistingRule
+               [ existingRule ]
+               newRule
+
+         Expect.isTrue
+            conflicts
+            "Should conflict when daily limit (60) between range (50,70)"
+
+         let newRule = {
+            newRule with
+               Criteria =
+                  Criteria.AmountPerCommand {
+                     LowerBound = Some 80m
+                     UpperBound = None
+                  }
+         }
+
+         let conflicts =
+            CommandApprovalRule.newRuleCriteriaConflictsWithExistingRule
+               [ existingRule ]
+               newRule
+
+         Expect.isTrue
+            conflicts
+            "Should conflict when lower bound (80) > existing rule's daily limit (60)"
+      }
    ]
