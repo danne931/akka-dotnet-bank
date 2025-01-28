6ea82ba92ae1a31c50c59fe898a47af6bfbac039
Daniel Eisenbarger <danne9.31@gmail.com>
2025-01-27 17:03:44 -0800

    fix sidebar menu call-to-action-remaining-approval-count not being updated due to out of sync context across separate component hierarchies

--- UI/src/App.fs
+++ UI/src/App.fs
@@ -13,6 +13,7 @@ open TransactionCategoryProvider
 open UserSessionProvider
 open MerchantProvider
 open OrgProvider
+open Bank.Employee.Domain
 
 [<ReactComponent>]
 let App () =
@@ -24,78 +25,67 @@ let App () =
       , [| box (string currentUrl) |]
    )
 
-   let activePage =
-      match currentUrl with
-      | Routes.IndexUrl.Analytics url ->
-         UserSessionSuspense(fun session ->
-            React.suspense (
-               [
-                  React.lazy' (
-                     fun () -> importDynamic "./Components/AnalyticsDashboard"
-                     , {| Session = session; Url = url |}
-                  )
-               ],
-               Html.progress []
-            ))
-         |> OrgProvider
-      | Routes.IndexUrl.Account url ->
-         AccountDashboard.AccountDashboardComponent url
-         |> UserSessionSuspense
-         |> OrgProvider
-      | Routes.IndexUrl.Approvals url ->
-         ApprovalDashboard.ApprovalDashboardComponent url
-         |> UserSessionSuspense
-         |> OrgProvider
-      | Routes.IndexUrl.Employees url ->
-         EmployeeDashboard.EmployeeDashboardComponent url
-         |> UserSessionSuspense
-         |> OrgProvider
-      | Routes.IndexUrl.EmployeeHistory url ->
-         EmployeeHistoryDashboard.EmployeeHistoryDashboardComponent url
-         |> UserSessionSuspense
-         |> OrgProvider
-      | Routes.IndexUrl.Cards url ->
-         CardDashboard.CardDashboardComponent url
-         |> UserSessionSuspense
-         |> OrgProvider
-      | Routes.IndexUrl.Transaction url ->
-         TransactionDashboard.TransactionDashboardComponent url
-         |> UserSessionSuspense
-         |> (OrgProvider
-             << SignalRConnectionProvider
-             << SignalRAccountEventProvider
-             << MerchantProvider
-             << TransactionCategoryProvider)
-      | Routes.IndexUrl.Payments url ->
-         PaymentDashboard.PaymentDashboardComponent url
-         |> UserSessionSuspense
-         |> OrgProvider
-      | Routes.IndexUrl.NotFound -> Html.h1 "Not Found"
+   let appShell (activePage: UserSession -> Fable.React.ReactElement) =
+      (fun (session: UserSession) ->
+         React.strictMode [
+            Navigation.NavigationComponent()
 
-   [
-      Navigation.NavigationComponent()
+            Html.div [
+               React.router [
+                  router.onUrlChanged (Routes.IndexUrl.parse >> setUrl)
+                  router.children [
+                     classyNode Html.div [ "container-fluid"; "app-shell" ] [
+                        classyNode Html.div [ "grid" ] [
+                           classyNode Html.aside [ "menu" ] [
+                              SidebarMenu.SidebarMenuComponent
+                                 currentUrl
+                                 session
+                           ]
 
-      Html.div [
-         React.router [
-            router.onUrlChanged (Routes.IndexUrl.parse >> setUrl)
-            router.children [
-               classyNode Html.div [ "container-fluid"; "app-shell" ] [
-                  classyNode Html.div [ "grid" ] [
-                     classyNode Html.aside [ "menu" ] [
-                        SidebarMenu.SidebarMenuComponent currentUrl
-                        |> UserSessionSuspense
-                        |> OrgProvider
+                           Html.section [ activePage session ]
+                        ]
                      ]
-
-                     Html.section [ activePage ]
                   ]
                ]
             ]
-         ]
-      ]
-   ]
-   |> React.strictMode
+         ])
+      |> UserSessionSuspense
+      |> OrgProvider
+      |> UserSessionProvider
+
+   match currentUrl with
+   | Routes.IndexUrl.Analytics url ->
+      appShell (fun session ->
+         React.suspense (
+            [
+               React.lazy' (
+                  fun () -> importDynamic "./Components/AnalyticsDashboard"
+                  , {| Session = session; Url = url |}
+               )
+            ],
+            Html.progress []
+         ))
+   | Routes.IndexUrl.Account url ->
+      appShell (AccountDashboard.AccountDashboardComponent url)
+   | Routes.IndexUrl.Approvals url ->
+      appShell (ApprovalDashboard.ApprovalDashboardComponent url)
+   | Routes.IndexUrl.Employees url ->
+      appShell (EmployeeDashboard.EmployeeDashboardComponent url)
+   | Routes.IndexUrl.EmployeeHistory url ->
+      appShell (EmployeeHistoryDashboard.EmployeeHistoryDashboardComponent url)
+   | Routes.IndexUrl.Cards url ->
+      appShell (CardDashboard.CardDashboardComponent url)
+   | Routes.IndexUrl.Transaction url ->
+      TransactionDashboard.TransactionDashboardComponent url
+      |> appShell
+      |> (SignalRConnectionProvider
+          << SignalRAccountEventProvider
+          << MerchantProvider
+          << TransactionCategoryProvider)
+   | Routes.IndexUrl.Payments url ->
+      appShell (PaymentDashboard.PaymentDashboardComponent url)
+   | Routes.IndexUrl.NotFound -> Html.h1 "Not Found"
 
 let root = ReactDOM.createRoot <| document.getElementById "bank-react-root"
 
-App() |> UserSessionProvider |> root.render
+App() |> root.render
