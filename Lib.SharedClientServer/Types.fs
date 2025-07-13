ab321aa2891390b82bc448aa9da3fa620c233ab2
Daniel Eisenbarger <danne9.31@gmail.com>
2025-07-13 09:34:01 -0700

    fix email type being validated on server but not in browser

--- Lib.SharedClientServer/Types.fs
+++ Lib.SharedClientServer/Types.fs
@@ -571,21 +571,29 @@ type Email = private {
 
    override x.ToString() = x.Email
 
+#if FABLE_COMPILER
+   static member private emailRegex =
+      Fable.Core.JS.Constructors.RegExp.Create @"^[^@\s]+@[^@\s]+\.[^@\s]+$"
+#endif
+
    static member ofString: Validator<string, Email> =
       fun field input ->
          let rule (x: string) =
             if String.IsNullOrEmpty x then
                false
-            elif String.length x > 255 then
-               false
             else
-#if FABLE_COMPILER
-               true
-#else
-               try
-                  (System.Net.Mail.MailAddress x).Address = x
-               with :? FormatException ->
+               let email = x.Trim()
+
+               if email.Length > 255 then
                   false
+               else
+#if FABLE_COMPILER
+                  Email.emailRegex.IsMatch email
+#else
+                  try
+                     (System.Net.Mail.MailAddress email).Address = email
+                  with :? FormatException ->
+                     false
 #endif
 
          let message = sprintf "%s must be a valid email address"
